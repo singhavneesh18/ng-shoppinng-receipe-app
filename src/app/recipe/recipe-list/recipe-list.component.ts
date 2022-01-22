5cabdf20ce99f2c9a5fe0041d1f418931dec2706
avneesh <singhtest18@gmail.com>
2022-01-22 19:24:34 +0000

    Updated local files to remote

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes:Recipe[] = [new Recipe('Recipe 1', 'Test Dec', 'https://th.bing.com/th/id/R.49d77df53f5da72f86202a0ec9b667e2?rik=mNaBDVogpk7OKQ&riu=http%3a%2f%2fstatic1.businessinsider.com%2fimage%2f554cbc9e69bedd0f1dd97cdf%2fgoogle-now-lets-you-order-food-delivery-right-from-its-search-results-pages.jpg&ehk=cllESkxpUIW8RfNcvClQwi1NPqkoQi9y9Wt2bkw9eMU%3d&risl=&pid=ImgRaw&r=0')];
+  recipes:Recipe[] = [new Recipe('Recipe 1', 'Test Dec', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')];
 
   constructor() { }
 
